9e3444c28b3bdbb9bfe0e2f61eaae91053bfbc07
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-02 07:53:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,8 @@
 require 'pry'
 def reverse_each_word(string)
   new_a = string.split(" ")
-  final_a =[]
-  index = 0
-  while index < new_a.length
-    final_a << new_a[index].reverse
-    index += 1
-    binding.pry
-  end
-  return final_a
+  final_a = []
+  new_a.each { |word| final_a << word.reverse }
+  sent = final_a.join(" ")
+  sent
 end
