cde3356c89eaefb14d9cead3d365833dbc51ba32
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-02 07:24:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
 def reverse_each_word(string)
-  
+  new_a = %w[string]
+  final_a =[]
+  index = 0
+  while index < new_a.length
+    final_a << new_a[index].reverse
+    index += 1
+  end
+  final_a = final_a.join(' ')
 
 end
